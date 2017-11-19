96ba246db86a368fe9273d5ce0f42d93ae0a260e
ozkrleal <ozkralm10@hotmail.com>
2017-11-19 23:07:12 +0100

    Untracked files (bash scripts) added succesfully to repo.

---
+++ support/enable.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+git submodule add git@github.com:ozkrleal/5inaRow-game-engine.git game-engine
+git submodule add git@github.com:ozkrleal/5inaRow-API-gateway.git api-gateway
+git submodule add git@github.com:ozkrleal/5inaRow-auth-user-management.git auth-user-management
+git submodule add git@github.com:ozkrleal/5inaRow-highscores.git highscores
+git submodule add git@github.com:ozkrleal/5inaRow-front-end.git front-end
+
+git submodule update --init
+
+
+sudo docker build -t 5inarow .
+
+sudo docker run -d -p 5000:5000 5inarow /bin/bash -i -t
+
+
---
+++ support/initreposcripts.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#start database
+sudo service mongod start
+
+#start init scripts of each service
+# highscores
+# authusermgmt
+# gameengine
+# apigateway
