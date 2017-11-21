701d5277485f398cebbf990f335328b9d9902cbc
ozkrleal <ozkralm10@hotmail.com>
2017-11-21 19:50:03 +0100

    Included the 4/4 init scripts.

--- support/initreposcripts.sh
+++ support/initreposcripts.sh
@@ -8,6 +8,8 @@ sudo service mongod start
 chmod +x /auth-user-management/my_commands.sh
 ./auth-user-management/my_commands.sh
 # gameengine
+chmod +x /game-engine/script.sh
+./game-engine/script.sh
 # apigateway
 chmod +x /api-gateway/commands_scripts.sh
 ./api-gateway/commands_scripts.sh
