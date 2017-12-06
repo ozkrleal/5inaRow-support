f0fb70876e4f3daeb7e2f10e487676c09e25ea23
ozkrleal <ozkralm10@hotmail.com>
2017-12-06 20:58:58 +0100

    2/4 init scripts working

--- support/initreposcripts.sh
+++ support/initreposcripts.sh
@@ -7,21 +7,29 @@
 # authusermgmt
 
 #NOT WORKING, NEED NEW BASH SCRIPT
-chmod +x /auth-user-management/auth_user_management/my_commands.sh
-/auth-user-management/auth_user_management/my_commands.sh 
+#chmod +x /auth-user-management/auth_user_management/my_commands.sh
+#/auth-user-management/auth_user_management/my_commands.sh 
+#cd /auth-user-management/auth_user_management/
+#npm install bindings --save
+#npm install --save bcrypt
+#node server.js
 
+#working
 # gameengine
 #chmod +x /game-engine/script.sh
 #cd /game-engine/
 #./script.sh 
 
+#working
 # apigateway
 #chmod +x /api-gateway/api_gateway/commands_scripts.sh
 #/api-gateway/api_gateway/commands_scripts.sh 
+cd /api-gateway/api_gateway/
+node index.js &
 
-
+#not working
 #highscores
 #cd /highscores/highscores 
-#node /server.js
+#node server.js
 
 #top
