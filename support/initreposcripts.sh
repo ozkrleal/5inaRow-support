bdeb02fb16bd65601e7fe5de32f9e50f73b60d90
ozkrleal <ozkralm10@hotmail.com>
2017-12-07 13:01:03 +0100

    dockerfile and init reposcripts now work. apache running, mongo running and all repo's working.

--- support/initreposcripts.sh
+++ support/initreposcripts.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 #start database
+service apache2 start
 mongod &
 
 #start init scripts of each service
@@ -9,28 +10,33 @@ mongod &
 #NOT WORKING, NEED NEW BASH SCRIPT
 #chmod +x /auth-user-management/auth_user_management/my_commands.sh
 #/auth-user-management/auth_user_management/my_commands.sh 
-#cd /auth-user-management/auth_user_management/
-#npm install bcrypt --save
-#node server.js
+cd /auth-user-management/auth_user_management/
+npm install bcrypt --save
+node server.js &
 
-
+echo "auth-usr finished"
 #npm install bindings --save
 
-
-#working
-# gameengine
-#chmod +x /game-engine/script.sh
-#cd /game-engine/
-#./script.sh 
-
+echo "Api starting"
 #working
 # apigateway
-#cd /api-gateway/api_gateway/
-#node index.js &
+cd /api-gateway/api_gateway/
+node index.js &
+echo "Api finished"
+
+echo "highscores starting"
 
 #almost working
 #highscores
 cd /highscores/highscores 
-node server.js
+node server.js  &
 
-#top
+echo "java starting"
+#working
+# gameengine
+chmod +x /game-engine/script.sh
+cd /game-engine/
+mvn spring-boot:run 
+echo "java finished" 
+
+
